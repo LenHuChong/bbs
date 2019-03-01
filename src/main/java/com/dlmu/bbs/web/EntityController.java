1412260b57207a29cff3fe190ee1da6e31eb1837
mbc <1357891052@qq.com>
2019-03-01 14:48:08 +0800

    完成用户控制类

---
+++ src/main/java/com/dlmu/bbs/web/EntityController.java
@@ -0,0 +1,10 @@
+package com.dlmu.bbs.web;
+
+import javax.swing.text.html.parser.Entity;
+import java.util.List;
+
+public interface EntityController {
+
+    String post();
+
+}
---
+++ src/main/java/com/dlmu/bbs/web/UserController.java
@@ -0,0 +1,45 @@
+package com.dlmu.bbs.web;
+
+import com.dlmu.bbs.domain.User;
+import com.dlmu.bbs.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserController {
+    @Autowired
+    private UserServiceImpl userService;
+    @RequestMapping(value = "/" ,method = RequestMethod.GET)
+    public List<User> getUserList(){
+       List<User> users= userService.getAllUsers();
+    return users ;
+    }
+    @RequestMapping(value="/", method=RequestMethod.POST)
+    public String postUser(@ModelAttribute User user){
+        userService.addUser(user);
+        return "Success!";
+    }
+    @RequestMapping(value="/{id}", method=RequestMethod.GET)
+    public User getUser(@PathVariable String id){
+        return userService.findById(id);
+    }
+    @RequestMapping(value="/{id}", method= RequestMethod.PUT)
+    public String putUser(@PathVariable String id, @ModelAttribute User user){
+        User user1=userService.findById(id);
+        user1.setAuthority(user.getAuthority());
+        user1.setAge(user.getAge());
+        userService.updataUser(user1);
+        return  "Success!";
+    }
+    @RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+    public String deleteUser(@PathVariable String id){
+        userService.removeById(id);
+        return "success";
+    }
+
+
+
+}
